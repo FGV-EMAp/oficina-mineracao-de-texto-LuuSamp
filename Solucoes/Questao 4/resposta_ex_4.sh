b1b092b3a26155a5e77da2ba52f0ecd7b1e33e58
Luciano Pereira Sampaio <luciano.sampaio2004@gmail.com>
2023-03-31 17:15:05 -0300

    Questão 4 feita

--- Solucoes/Questao 4/resposta_ex_4.sh
+++ Solucoes/Questao 4/resposta_ex_4.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "$(cat ../../ColdplayAlbum/*.txt | tr ' ' '\n' |sed 's/,//g' | sed 's/(//g' | sed 's/)//g' | sort -u | uniq -i | wc -w)" > resultado.txt
+cat ../../ColdplayAlbum/*.txt | grep -o '\w*' | sort | uniq -c -i > resultado.txt
